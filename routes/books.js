ad96cf71fb3c8a680f68051f83bee315a0008824
Gabriel Isaac <stewartisaac90@gmail.com>
2024-06-15 19:13:05 +0100

    adding books and categories routes and models

---
+++ routes/books.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const Book = require("../models/Books.js");
+const BookCategory = require("../models/BookCategory.js");
+
+const router = express.Router()
+
+// Get all Books in the Database
+router.get("/allbooks", async (req, res) => {
+    try {
+        const books = await Book.find({}).populate("transactions").sort({ _id: -1 })
+        res.status(200).json(books)
+    }
+    catch (err) {
+        return res.status(504).json(err);
+    }
+})
+
+// Get Book by Book ID
+router.get("/getbook/:id", async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id).populate("transactions")
+        res.status(200).json(book)
+    }
+    catch {
+        return res.status(500).json(err)
+    }
+})
+
+// Get books by Category Name
+router.get("/", async (req, res) => {
+    const category = req.query.category
+    try {
+        const books = await BookCategory.findOne({ categoryName: category }).populate("books")
+        res.status(200).json(books)
+    }
+    catch (err) {
+        return res.status(504).json(err)
+    }
+})
+
+// Adding book
+router.post("/addbook", async (req, res) => {
+    if (req.body.isAdmin) {
+        try {
+            const newbook = await new Book({
+                title: req.body.title,
+                author: req.body.author,
+                bookISBN: req.body.bookISBN,
+                categories: req.body.categories,
+                availableCopies: req.body.availableCopies,
+                publisher: req.body.publisher,
+                
+            })
+            const book = await newbook.save()
+            await BookCategory.updateMany({ '_id': book.categories }, { $push: { books: book._id } });
+            res.status(200).json(book)
+        }
+        catch (err) {
+            res.status(504).json(err)
+        }
+    }
+    else {
+        return res.status(403).json("You dont have permission to delete a book!");
+    }
+})
+
+// Updating book
+router.put("/updatebook/:id", async (req, res) => {
+    if (req.body.isAdmin) {
+        try {
+            await Book.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("Book details updated successfully");
+        }
+        catch (err) {
+            res.status(504).json(err);
+        }
+    }
+    else {
+        return res.status(403).json("You dont have permission to delete a book!");
+    }
+})
+
+// Remove book
+router.delete("/removebook/:id", async (req, res) => {
+    if (req.body.isAdmin) {
+        try {
+            const _id = req.params.id
+            const book = await Book.findOne({ _id })
+            await book.remove()
+            await BookCategory.updateMany({ '_id': book.categories }, { $pull: { books: book._id } });
+            res.status(200).json("Book has been deleted");
+        } catch (err) {
+            return res.status(504).json(err);
+        }
+    } else {
+        return res.status(403).json("You dont have permission to delete a book!");
+    }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-// Import Modules
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
-// App Configurations
+const bookRoutes = require("./routes/books");
+const categoryRoutes = require("./routes/categories");
+
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8000
 
-// MongoDB connection
+app.use("/api/books", bookRoutes);
+app.use("/api/categories", categoryRoutes);
+
+
 mongoose.connect(
   process.env.MONGODB_URL,
   {
@@ -20,91 +25,19 @@ mongoose.connect(
   console.log("MongoDB Successfully Connected")
 );
 
-// Middlewares
 app.use(express.json());
 app.use(bodyParser.json() );      
     app.use(bodyParser.urlencoded({    
          extended: true
 }));
 
-const books = [{
-  bookName: "Rudest Book Ever",
-  bookAuthor: "Shwetabh Gangwar",
-  bookPages: 200,
-  bookPrice: 240,
-  bookState: "Available"
-},
-{
-  bookName: "Do Epic Shit",
-  bookAuthor: "Ankur Wariko",
-  bookPages: 200,
-  bookPrice: 240,
-  bookState: "Available"
-}
-]
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to YouThrive Library Management App");
+});
 
 
-app.get('/', (req, res) => {
-  res.status(200).json({data: books})
-})
-
-// Handling form data
-app.post('/addBook', (req, res) => {
-  const inputBookName = req.body.bookName
-  const inputBookAuthor = req.body.bookAuthor
-  const inputBookPages = req.body.bookPages
-  const inputBookPrice = req.body.bookPrice
-  
-  books.push({
-      bookName: inputBookName,
-      bookAuthor: inputBookAuthor,
-      bookPages: inputBookPages,
-      bookPrice: inputBookPrice,
-      bookState: "Available"
-  })
-  
-  res.status(200).json({data: books})
-})
-
-// Issuing a Book
-app.post('/issue', (req, res) => {
-  var requestedBookName = req.body.bookName;
-  books.forEach(book => {
-      if (book.bookName == requestedBookName) {
-          book.bookState = "Issued";
-      }
-  })
-  res.status(200).json({
-      data: books
-  })
-})
-
-// Returning a Book
-app.post('/return', (req, res) => {
-  var requestedBookName = req.body.bookName;
-  books.forEach(book => {
-      if (book.bookName == requestedBookName) {
-          book.bookState = "Available";
-      }
-  })
-  res.json({data: books})
-})
-
-// Deleting a Book
-app.post('/delete', (req, res) => {
-  var requestedBookName = req.body.bookName;
-  var j = 0;
-  books.forEach(book => {
-      j = j + 1;
-      if (book.bookName == requestedBookName) {
-          books.splice((j - 1), 1)
-      }
-  })
-  res.json({data: books})
-})
-
-
-
+/* ---------------------------- */
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 })
